f037d2849722775de0d4e95a330270a0c8f98ffd
zhanna-dmit <my.freedpm95@gmail.com>
2026-01-19 20:03:59 +0300

    Add configuration module

---
+++ configuration.py
@@ -0,0 +1,14 @@
+# хранит базовый URL веб-сервиса
+URL_SERVICE = "https://c57a1526-87d4-43f0-9eea-943fc376abd2.serverhub.praktikum-services.ru"
+
+# путь к документации веб-сервиса
+DOC_PATH = "/docs/"
+
+# путь до таблицы user
+USERS_TABLE_PATH = "/api/db/resources/user_model.csv"
+
+# путь для создания нового пользователя.
+CREATE_USER_PATH = "/api/v1/users/"
+
+# путь для создания набора пользователя
+CREATE_USER_KITS = "/api/v1/kits"
